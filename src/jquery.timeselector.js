b1a7dfa545fe2555d53eef6b9a8472adfa10fdf1
Nicolas Zhao <nicolas-zhao@hotmail.com>
2014-01-09 10:04:03 +0800

    rename plugin file name

---
+++ src/jquery.timeselector.js
@@ -0,0 +1,21 @@
+/*
+ * timeselector
+ * https://github.com/nicolaszhao/timeselector
+ *
+ * Copyright (c) 2014 Nicolas Zhao
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+	
+	$.fn.timeselector = function() {
+		return this;
+	};
+	
+	$.fn.timeselector.defaults = {
+		
+	};
+	
+	$.fn.timeselector.Timeselector = function() {};
+
+}(jQuery));
